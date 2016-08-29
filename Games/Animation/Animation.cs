fc247d3f55f44808c147abcefea390844e81d9f7
Aleksey <lehazakharov@yandex.ru>
2016-08-29 08:51:02 +0300

    Animation begin

---
+++ Games/Animation/Animation.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animation
+{
+    class Animation
+    {
+        Texture2D texture;
+        Rectangle position;
+
+        int framesCount;
+
+        int millisecondaPerFrame;
+        int currentGameTime = 0;
+
+        int currentFrame = 0;
+
+        public Animation(Texture2D newTexture, Rectangle newRectangle, int newFramesCount, int newMillisecobdsPerFrame)
+        {
+            //texture = newTexture;
+            //rectangle = newRectangle;
+            //framesCount = newFramesCount;
+            //millisecondaPerFrame = newMillisecobdsPerFrame;
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            //Rectangle destinationRectangle = new Rectangle(rectangle.X, rectangle.Y, texture.Width / framesCount, texture.Height);
+            //Rectangle sourceRectangle = new Rectangle()
+
+            //spriteBatch.Draw(texture, )
+        }
+
+    }
+
+    class AnimationExecution
+    {
+        Animation animation;
+
+        AnimationExecution(Animation newAnimation, int newMillisecondsPerFrame)
+        {
+            animation = newAnimation;
+        }
+
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            
+
+        }
+    }
+}
---
+++ Games/SkrollingScreen/Background.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkrollingScreen
+{
+    class Scrolling
+    {
+        // Left texture to diaplay and it's position
+        Texture2D first;
+        Rectangle firstRectangle;
+
+        // Right texture to diaplay and it's position
+        Texture2D second;
+        Rectangle secondRectangle;
+
+        // Velocity with witch screen scrolling
+        int screenMoveVelocity = 10;
+
+        /// <summary>
+        /// Constrictor with one parameter 
+        /// - set picture on background 
+        /// </summary>
+        /// <param name="texture"> Texture on background </param>
+        public Scrolling(Texture2D texture)
+        {
+            // set first background
+            first = texture;
+            firstRectangle = new Rectangle(0, 0, first.Width, first.Height);
+
+            // set second background
+            second = texture;
+            secondRectangle = new Rectangle(first.Width, 0, second.Width, second.Height);
+        }
+
+        /// <summary>
+        /// Update the screen position by executing scrolling process
+        /// </summary>
+        public void Update()
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                // Move screen with screen velocity 
+                firstRectangle.X -= screenMoveVelocity;
+                secondRectangle.X -= screenMoveVelocity;
+
+                // Scrolling conditions
+                if (firstRectangle.X + first.Width <= 0)
+                    firstRectangle.X = secondRectangle.X + secondRectangle.Width;
+
+                if (secondRectangle.X + second.Width <= 0)
+                    secondRectangle.X = firstRectangle.X + firstRectangle.Width;
+            }
+            else if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                // Move screen with screen velocity 
+                firstRectangle.X += screenMoveVelocity;
+                secondRectangle.X += screenMoveVelocity;
+
+                // Scrolling conditions
+                if (firstRectangle.X >= 0)
+                    secondRectangle.X = firstRectangle.X - secondRectangle.Width;
+
+                if (secondRectangle.X >= 0)
+                    firstRectangle.X = secondRectangle.X - firstRectangle.Width;
+
+
+            }
+        }
+
+        /// <summary>
+        /// Draw screen
+        /// </summary>
+        /// <param name="spriteBatch"> sprite batch</param>
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(first, firstRectangle, Color.White);
+            spriteBatch.Draw(second, secondRectangle, Color.White);
+        }
+    }
+}
