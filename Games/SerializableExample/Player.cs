9a61639b7d92e7d34829ce7a307dea5aefbb83d6
ZakharovAleksey <lehazakharov@hotmail.com>
2016-09-16 17:54:19 +0300

    Seriazable to XML project
    Create project

---
+++ Games/SerializableExample/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializableExample
+{
+    class Player
+    {
+        float positionX;
+        float positionY;
+
+        int health;
+        bool isAlive;
+
+        Player() { }
+
+    }
+}
